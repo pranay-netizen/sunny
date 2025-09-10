edc7f84e77a7571d04ed0792f78ce9651a38f6a6
pranay <pranayb506@gmail.com>
2025-09-10 21:16:41 +0530

    new file

---
+++ sunny.py
@@ -0,0 +1,31 @@
+# String creation
+name = "Pranay"
+greeting = 'Hello'
+multiline = """This is 
+a multiline 
+string."""
+
+# String concatenation
+message = greeting + " " + name
+print("Concatenation:", message)
+
+# String repetition
+repeat = name * 3
+print("Repetition:", repeat)
+
+# String indexing
+print("First character:", name[0])
+print("Last character:", name[-1])
+
+# String slicing
+print("First three letters:", name[:3])
+print("Last three letters:", name[-3:])
+
+# String methods
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("Title Case:", "welcome to python".title())
+print("Replaced:", name.replace("a", "@"))
+
+# Check membership
+print("Contains 'na
